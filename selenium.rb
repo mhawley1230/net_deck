1d73395ef68da8ecdbb3dcb55060d90ca107de51
mhawley1230@gmail.com <Mike>
2017-01-27 18:06:33 -0500

    modified selenium algorithm

--- selenium.rb
+++ selenium.rb
@@ -11,20 +11,27 @@ driver.navigate.to 'http://mtgtop8.com/format?f=ST'
   t = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[2]/table[2]/tbody/tr[#{i+2}]/td[1]/a")
   t.click
 
-  tournament_name = driver.find_element(:xpath, '/html/body/div[3]/div/div/table/tbody/tr/td[1]').text
-  tournament_info = driver.find_element(:xpath, '/html/body/div[3]/div/table/tbody/tr/td[1]/div/table/tbody/tr/td').text
-  t = tournament_info.split()
-  tournament_format = t[0]
-  tournament_no_of_players = t[1]
-  tournament_date = t[4]
+  tournament_data_container = driver.find_element(:xpath, '/html/body/div[3]/div/table/tbody/tr/td[1]/div')
+  deck_links = tournament_data_container.find_elements(:xpath, 'div/div/a')
+  deck_links.each do |link|
+    puts link.text
+  end
+  deck_length = deck_links.length / 2
 
-  8.times do |i|
+  # tournament_name = driver.find_element(:xpath, '/html/body/div[3]/div/div/table/tbody/tr/td[1]').text
+  # tournament_info = driver.find_element(:xpath, '/html/body/div[3]/div/table/tbody/tr/td[1]/div/table/tbody/tr/td').text
+  # t_data = tournament_info.split()
+  # tournament_format = t_data[0]
+  # tournament_no_of_players = t_data[1]
+  # tournament_date = t_data[4]
+
+  deck_length.times do |i|
     results = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[1]/div/div[#{i+1}]/div[2]/a")
     results.click
 
     deck_placing = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[1]/div/div[#{i+1}]/div[1]").text
     deck_name = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[1]/div/div[#{i+1}]/div[2]/a").text
-    deck_player = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[1]/div/div[#{i+1}]/div[3]/a").text
+    player = driver.find_element(:xpath, "/html/body/div[3]/div/table/tbody/tr/td[1]/div/div[#{i+1}]/div[3]/a").text
 
     decklist = driver.find_elements(class_name: 'G14')
     decklist.each do |card|
@@ -34,12 +41,13 @@ driver.navigate.to 'http://mtgtop8.com/format?f=ST'
       split_name = split_text[1..-1]
       card_name = split_name.join(' ')
 
-      i = 0
-      card_img = MTG::Card.where(name: card_name).all[i].image_url
-      if card_img.empty?
-        i += 1
+      while card_img = nil
+        i = 0
+        card_img = MTG::Card.where(name: card_name).all[i].image_url
+        if card_img == nil
+          i += 1
+        end
       end
-      binding.pry
 
       if MTG::Card.where(name: card_name).all[0].colors.nil?
         card_colors = 'Colorless'
